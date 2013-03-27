88aecdaade27d3a40e35722f7990b18b858a3b22
Arief 41 <arifwigunaa@gmail.com>
2013-03-27 10:31:33 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,27 @@
+  				<ul>
+						<form action="proses_login.php" method="post" form name="form1">
+  						<div align="center">
+    					<table width="319" border="1" align="center">
+     				    <tr>
+       				    <td width="111">Username</td>
+        				<td width="7">:</td>
+       				    <td width="179"><label>
+          				<input name="username" type="text" id="username" />
+        				</label></td>
+      					</tr>
+      					<tr>
+       				    <td>Password</td>
+       				    <td>:</td>
+         			    <td><label>
+         			    <input name="password" type="password" id="password" />
+        				</label></td>
+      					</tr>
+    					</table>
+ 					    </div>
+  						<p align="center">
+   					    <label>
+   					    <input name="Login" type="submit" id="Login" value="Login" />
+    					</label>
+  						</p>
+						</form>
+					</ul>
