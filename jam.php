d1e8146da0c60d682a27e00459505095d9c56aaf
Arief 41 <arifwigunaa@gmail.com>
2013-03-27 14:14:43 +0700

    Create jam.php

---
+++ jam.php
@@ -0,0 +1,17 @@
+<div style="display:inline-block;">
+  <script type="text/javascript" src="http://www.webestools.com/page/js/flashobject.js"></script>
+    <div id="horloge_947026331" style="display:inline-block;">
+		<a href="http://get.adobe.com/fr/flashplayer/">You need to install the Flash plugin</a> - <a href="http://www.webestools.com/">Webestools</a>
+    </div>
+    <script type="text/javascript">
+    //<!--
+		var flashvars_947026331 = {};
+		var params_947026331 = {
+				quality: "high",
+				wmode: "transparent"
+			};
+		var attributes_947026331 = {};
+        flashObject("http://flash.webestools.com/horloges/horloge15.swf", "horloge_947026331", "150", "150", "8", false, flashvars_947026331, params_947026331, attributes_947026331);
+    //-->
+    </script>
+</div>
