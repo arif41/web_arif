c59949e42c04b8e2e30c71f1f3167ebcb24c6dc5
Arief 41 <arifwigunaa@gmail.com>
2013-03-27 10:34:27 +0700

    Create response.php

---
+++ response.php
@@ -0,0 +1,18 @@
+<blockquote>&nbsp;  </blockquote>
+<table width="495" border="1" align="center">
+
+  <?php
+  require "config.php";
+  $data=mysql_query("select * from chat");
+  $no=1;
+  while ($tampil=mysql_fetch_array($data)) {
+  $username=$tampil["username"];
+  $gambar=$tampil['gambar'];
+  $chat=$tampil['chat'];
+  ?>
+ 	<tr><td height="104"><img src="dirchat/<?php echo $gambar; ?>" width="100" height="100"></td>
+    <td><?php echo $username; ?></td>
+    <td><?php echo $chat; ?></td>
+ </tr>
+  <?php $no++;  } ?>
+</table>
