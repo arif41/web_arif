d4361d414a6cb1a2b4028bf7cf2f3828e7e4071c
Arief 41 <arifwigunaa@gmail.com>
2013-03-27 10:35:16 +0700

    Create tambah_barang.php

---
+++ tambah_barang.php
@@ -0,0 +1,8 @@
+<form action="?menu=input" method="post" enctype="multipart/form-data" name="form1">
+  <label> Masukan Jumlah Data Yang Akan Di Input <br>
+  <input name="jum" type="text" id="jum">
+  </label>
+  <label>
+  <input type="submit" name="Submit" value="Submit">
+  </label>
+</form>
