376718dbb8650634134073b887fea7268f8960a9
Arief 41 <arifwigunaa@gmail.com>
2013-03-27 10:24:58 +0700

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,17 @@
+<table width="373" border="1">
+  <tr>
+    <td width="122">Nama</td>
+    <td width="8">:</td>
+    <td width="226">Arif Wiguna </td>
+  </tr>
+  <tr>
+    <td>Alamat</td>
+    <td>:</td>
+    <td>Jl.Randu Sari Raya RT.04 RW.17 Antapani Kidul , Bandung </td>
+  </tr>
+  <tr>
+    <td>No.Tlp</td>
+    <td>:</td>
+    <td>+628995636319</td>
+  </tr>
+</table>
