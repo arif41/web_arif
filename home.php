59a30ee6a178d6e54852672b30dd081dd6df83b6
Arief 41 <arifwigunaa@gmail.com>
2013-03-27 10:29:47 +0700

    Create home.php

---
+++ home.php
@@ -0,0 +1,10 @@
+<style type="text/css">
+<!--
+.style1 {
+  font-family: Georgia, "Times New Roman", Times, serif;
+	font-weight: bold;
+}
+-->
+</style>
+<h1 class="title">Welcome to CMS Shop!</h1>
+<p class="title">Solusi untuk tutup pentil motor anda! </p>
