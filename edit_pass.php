227f0acf3e07b52c9dc58b85614af2398da12409
Arief 41 <arifwigunaa@gmail.com>
2013-03-27 14:15:55 +0700

    Update edit_pass.php

--- edit_pass.php
+++ edit_pass.php
@@ -1,21 +1,23 @@
 <?php
 require "config.php";
 $user=$_SESSION['username'];
-$data=mysql_query("select * from password where username='$user' ");
+$data=mysql_query("select * from password where username='$user'");
 $tampil=mysql_fetch_array($data);
+$username=$tampil['username'];
 $password=$tampil['password'];
 ?>
 
 <form action="proses_pass.php" method="post" enctype="multipart/form-data" name="form1">
 <table width="424" border="1">
-  <tr>
+    <tr>
     <td width="103">Password Baru</td>
     <td width="8">:</td>
     <td width="291"><label>
-        <input name="password" type="password" id="password" size="47">
-      </label></td>
+    <input name="pass" type="password" id="pass" size="47">
+    </label></td>
+	</tr>
 </table>
-<input name="username" type="hidden" id="username" value="<?php echo $username; ?>">
+<input name="username" type="hidden" id="username" value="<?php echo $user; ?>">
 <p>
   <label>
   <input type="submit" name="Submit" value="Submit">
