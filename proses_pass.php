bfa01bfe9efa1b664bc6f2f6dc89bae4b0172506
Arief 41 <arifwigunaa@gmail.com>
2013-03-28 17:24:25 +0700

    Update proses_pass.php

--- proses_pass.php
+++ proses_pass.php
@@ -2,10 +2,10 @@
 require "config.php";
 
 //data
-$username=$_POST['username'];
+$user=$_POST['username'];
 $password=md5($_POST['pass']);
 
 //proses ke database
-$upload_database=mysql_query("update password set username='$username',password='$pass' where username='$user'");
+$upload_database=mysql_query("update password set username='$user',password='$pass' where username='$user'");
 if ($upload_database){echo "<script> alert('Berhasil Ganti Password'); document.location.href='index.php?menu=home'; </script>";} else { echo "<script>alert('Gagal Ganti'); document.location.href='index.php?menu=home'; </script>";}
 ?>
