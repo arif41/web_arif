48022102d04cdef99ae9ba9693874e8efa21f46b
Arief 41 <arifwigunaa@gmail.com>
2013-03-28 12:48:51 +0800

    Create back-to-top.php

---
+++ back-to-top.php
@@ -0,0 +1,49 @@
+Hello World
+<br>
+<br>
+<br>
+<br>
+<br>
+<br><br>
+<br>
+<br><br>
+<br>
+<br><br>
+<br>
+<br><br>
+<br>
+<br><br>
+<br>
+<br><br>
+<br>
+<br><br>
+<br>
+<br><br>
+<br>
+<br><br>
+<br>
+<br><br>
+<br>
+<br><br>
+<br>
+<br><br>
+<br>
+<br><br>
+<br>
+<br><br>
+<br>
+<br><br>
+<br>
+<br><br>
+<br>
+<br>
+<a id="top" href="#">Back to top!</a>
+<script src='http://code.jquery.com/jquery-1.9.1.js'></script>
+<script>
+$(function() {
+    $('#top').click(function() {
+        $('html, body').animate({scrollTop:0}, 'slow');
+        return false;
+    });
+});
+</script>
